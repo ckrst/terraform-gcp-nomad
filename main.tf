d5c0b37c8e29dff4700f4309c25dc9aa8ad1b4a6
ckrst <58567534+ckrst@users.noreply.github.com>
2024-02-05 14:36:44 -0300

    instance template

--- main.tf
+++ main.tf
@@ -10,3 +10,35 @@ resource "google_compute_instance_group" "instance_group" {
   zone        = var.google_default_zone
   #   network     = google_compute_network.default.id
 }
+
+data "google_compute_image" "my_image" {
+  family  = var.nomad_image_family
+  project = var.nomad_image_project
+}
+
+resource "google_compute_instance_template" "server_instance_template" {
+  name           = "nomad-server-instance-template"
+  machine_type   = "e2-micro"
+
+  can_ip_forward = false
+
+  tags = ["foo", "bar"]
+
+  disk {
+    source_image = data.google_compute_image.my_image.id
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "\n echo '\"runlist\":[\"nomad_cookbook\",\"nomad_cookbook[server]\"]' > /tmp/nomad.json \n chef-solo -j /tmp/nomad.json" 
+  
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
