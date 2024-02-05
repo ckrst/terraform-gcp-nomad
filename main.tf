5aa0c4ba89fe53bb9c28e8f5c075eba24d2f34e8
ckrst <58567534+ckrst@users.noreply.github.com>
2024-02-04 22:12:40 -0300

    instance group

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "google" {
+  project = var.google_project
+  region = var.google_default_region
+  credentials = var.google_credentials
+}
+
+resource "google_compute_instance_group" "instance_group" {
+  name        = "nomad-ig"
+  description = "Nomad instance group"
+  zone        = var.google_default_zone
+#   network     = google_compute_network.default.id
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+
+variable "google_credentials" {}
+variable "google_project" {}
+variable "google_default_region" { default = "us-central1" }
+variable "google_default_zone" { default = "us-central1-a" }
