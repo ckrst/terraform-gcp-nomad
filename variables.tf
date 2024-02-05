bb445cc0251354322d65a539ef92f0a3743597f2
ckrst <58567534+ckrst@users.noreply.github.com>
2024-02-05 14:37:12 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,6 @@ variable "google_credentials" {}
 variable "google_project" {}
 variable "google_default_region" { default = "us-central1" }
 variable "google_default_zone" { default = "us-central1-a" }
+
+variable "nomad_image_family" { default = ""}
+variable "nomad_image_project" {}
